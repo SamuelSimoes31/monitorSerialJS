cc57a8e72f3667bc65ecd5284582158e63aca3b4
Samuel Simões <samuelsimoes31@gmail.com>
2021-01-05 19:34:34 -0300

    feat(write): byte mode switch button added

--- frontend/src/components/SerialWrite/index.js
+++ frontend/src/components/SerialWrite/index.js
@@ -4,6 +4,7 @@ import {useGeneralContext} from '../GeneralContext';
 export default function SerialRead() {
   const { socket, portIsOpen } = useGeneralContext()
   const [writeMessages,setWriteMessages] = useState('OXE')
+  const [byteMode,setByteMode] = useState(false)
 
   function handleSend(){
     socket.emit('writeSerial',writeMessages)
@@ -11,13 +12,26 @@ export default function SerialRead() {
 
   return(
     <div className="container">
-      <textarea
-        rows={5}
-        cols={80}
-        value={writeMessages}
-        onChange={e => setWriteMessages(e.target.value)}
-        disabled={ portIsOpen ?'':'disabled'}
-      />
+      {
+        byteMode ?
+        <div className="byteModeContainer">
+          <p>QUADRADO</p>
+        </div>
+        :
+        <div>
+          <textarea
+          rows={5}
+          cols={80}
+          value={writeMessages}
+          onChange={e => setWriteMessages(e.target.value)}
+          disabled={ portIsOpen ?'':'disabled'}
+          />
+        </div>
+      }
+      
+      <button onClick={() => { setByteMode(!byteMode)}}>
+        {byteMode?"Switch to ASCII mode":"Switch to byte mode"}
+      </button>
       <button onClick={handleSend}>SEND</button>
     </div>
   )
