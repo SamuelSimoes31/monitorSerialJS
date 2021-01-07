be19f2bc05933d783af108fae56e5e74761e873e
Samuel Simões <samuelsimoes31@gmail.com>
2021-01-06 21:13:35 -0300

    feat(write): byte box form, form prototype added

--- frontend/src/components/ByteBox/index.js
+++ frontend/src/components/ByteBox/index.js
@@ -2,19 +2,33 @@ import React, {useState} from 'react';
 
 import './styles.css'
 
+function ByteBoxForm() {
+  return (
+    <div className="byte-box-form container">
+      AAAAAAAAAAAAAA
+    </div>
+  )
+}
+
 export default function ByteBox(){
   const [number, setNumber] = useState(255);
   const [base, setBase] = useState(10);
+
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <div className="byte-number-container">
-      <p>{number ? number.toString(base) : '+'}</p>
-      <p>{
-        number && (
-          base === 10 ? 'dec' :
-          base === 2 ? 'bin' :
-          base === 16 ? 'hex' : 'oct'
-        )
-        }</p>
-    </div>
+    <>
+      <div className="byte-number-container" onClick={() => setShowForm(!showForm)}>
+        <p>{number ? number.toString(base) : '+'}</p>
+        <p>{
+          number && (
+            base === 10 ? 'dec' :
+            base === 2 ? 'bin' :
+            base === 16 ? 'hex' : 'oct'
+          )
+          }</p>
+      </div>
+      {showForm && <ByteBoxForm/>}
+    </>
   )
 }
